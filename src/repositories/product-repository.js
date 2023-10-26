359da02a65af02a7dc23a17773c20bf86abd3e55
Olivio Neto <netoolivio34@gmail.com>
2023-10-26 11:06:00 -0300

    Criando as rotas dos produtos

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -9,3 +9,23 @@ exports.get = () => {
             active: true
         }, 'title price slug');
 }
+
+exports.getBySlug = (slug) => {
+    return Product
+    .findOne({
+        slug: slug,
+        active: true}, 'title description price slug tags');
+}
+
+exports.getById = (id) => {
+    return Product
+    .findById(id);
+}
+
+exports.getByTag = (tag) => {
+    return Product
+    .find({
+        tags: tag,
+        active: true
+    }, 'title description price slug tags');
+}
